d9a2622302e6ba2dac6e0e618a51870699bc9278
Brenno Rodrigues <brennopereira6@gmail.com>
2024-03-29 02:18:59 -0300

    feat: adding an evaluation with mutable context

--- src/bronen/cljisp.clj
+++ src/bronen/cljisp.clj
@@ -30,5 +30,38 @@
 (defn parse-expression
   "Parses tokens to syntax tree"
   [[token & tokens]]
-  (println token tokens)
   (parse-list token tokens))
+
+(def default-context
+  {"def" (fn [v] {:token "definition" :value v})})
+
+(defn eval-literal
+  [{token :token value :value} ctx]
+  (when (= token "literal")
+    (let [default-value (get default-context value)]
+      (if (nil? default-value)
+        (get @ctx value)
+        (default-value value)))))
+
+(defn eval-number
+  [{token :token value :value} ctx]
+  (if (= token "number")
+    value
+    (eval-literal {:token token :value value} ctx)))
+
+(defn eval-string
+  [{token :token value :value} ctx]
+  (if (= token "string")
+    value
+    (eval-number {:token token :value value} ctx)))
+
+(defn evaluate
+  "Evaluates a syntax tree"
+  [{token :token value :value} ctx]
+  (if (= token "list")
+    (let [f (evaluate (first value) ctx)]
+      (if (= (:token f) "definition")
+        (swap! ctx #(conj % {(:value (get value 1))
+                             (:value (get value 2))}))
+        (println "error: calling a not callable")))
+    (eval-string {:token token :value value} ctx)))
--- test/bronen/cljisp_test.clj
+++ test/bronen/cljisp_test.clj
@@ -1,21 +1,58 @@
 (ns bronen.cljisp-test
   (:require [clojure.test :refer [deftest testing is]]
-            [bronen.cljisp :refer [lexer parse-expression]]))
+            [bronen.cljisp :refer [lexer parse-expression, evaluate]]))
 
 (deftest lexer-test
   (testing "TODO: implement lexer"
     (is (= (lexer "") nil))))
 
 (deftest parser-test
-  (testing "TODO: implement parser"
+  (testing "Should parse syntax tokens into a valid syntax tree"
+    (is (= (parse-expression [{:token "literal" :value "println"}])
+           {:token "literal" :value "println"}))
     (is (= (parse-expression [{:token "number" :value "5"}])
            {:token "number" :value 5}))
     (is (= (parse-expression [{:token "string" :value "lorem ipsum"}])
            {:token "string" :value "lorem ipsum"}))
     (is (= (parse-expression [{:token "lbraces"}
+                              {:token "literal" :value "println"}
                               {:token "number" :value "5"}
                               {:token "string" :value "lorem ipsum"}
                               {:token "rbraces"}])
            {:token "list"
-            :value [{:token "number" :value 5}
+            :value [{:token "literal" :value "println"}
+                    {:token "number" :value 5}
                     {:token "string" :value "lorem ipsum"}]}))))
+
+(deftest eval-test
+  (testing "Should evaluate a syntax tree and return a value"
+    (is (= (evaluate {:token "literal" :value "example"} (atom {"example" 123})) 123))
+    (is (= (evaluate {:token "number" :value 5} (atom {})) 5))
+    (is (= (evaluate {:token "string" :value "lorem ipsum"} (atom {})) "lorem ipsum")))
+  (testing "Should evaluate a syntax tree and mutate the original context"
+    (let [ctx (atom {"example" 123})]
+      (evaluate
+       {:token "list"
+        :value [{:token "literal" :value "def"}
+                {:token "literal" :value "wasd"}
+                {:token "number" :value 333}]}
+       ctx)
+      (is (= @ctx {"example" 123 "wasd" 333})))))
+
+;(is (= (parse-expression [{:token "lbraces"}
+;                              {:token "string" :value "lorem ipsum"}
+;                              {:token "lbraces"}
+;                              {:token "lbraces"}
+;                              {:token "literal" :value "println"}
+;                              {:token "number" :value "123"}
+;                              {:token "rbraces"}
+;                              {:token "number" :value "234"}
+;                              {:token "rbraces"}
+;                              {:token "rbraces"}])
+;           {:token "list"
+;            :value [{:token "string" :value "lorem ipsum"}
+;                    {:token "list"
+;                     :value [{:token "list"
+;                              :value [{:token "literal" :value "println"}
+;                                      {:token "number" :value 123}]}]}
+;                    {:token "number" :value 234}]}))
